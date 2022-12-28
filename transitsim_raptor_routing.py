857f52487fce6957de2c892886be7dec46080cf4
Reid Passmore <tpassmore6@gatech.edu>
2022-12-28 11:50:06 -0500

    back to pc

--- transitsim_raptor_routing.py
+++ transitsim_raptor_routing.py
@@ -90,16 +90,9 @@ def run_raptor(all_trips,impedance,raptor_settings):
             #import trips file
             trips = pd.read_parquet(trip)
             
-            if raptor_settings['one_trip'] == True:
-                start = raptor_settings['start']
-                end = raptor_settings['end']
-                trips = trips.loc[trips['src_taz']==start and trips['dest_taz']==end,:]
-            
             if trips.shape[0] == 0:
-                if raptor_settings['one_trip'] == True:
-                    return
-                else:
-                    print(f'No trips possible from {trip}')
+                print(f'No trips possible from {trip}')
+            
             else:
             
                 #get start taz name
@@ -224,9 +217,133 @@ def run_raptor(all_trips,impedance,raptor_settings):
             # with open(r'C:\Users\tpassmore6\Documents\TransitSimData\Data\exclude_trips.pkl','wb') as fh:
             #     pickle.dump(exclude_routes,fh)
     
+
 #make a one trip version
-def one_trip(start_taz,end_taz,D_TIME,raptor_settings):
+def one_trip(trips,impedance,raptor_settings,start_time):
     
+    #import network files
+    stops_file, trips_file, stop_times_file, transfers_file, stops_dict, stoptimes_dict, footpath_dict, routes_by_stop_dict, idx_by_route_stop_dict = read_testcase(
+        raptor_settings['NETWORK_NAME'])
+
+    #print details
+    print_network_details(transfers_file, trips_file, stops_file)
+    
+    #get variables
+    bike_thresh = raptor_settings['bike_thresh']
+    bikespd = raptor_settings['bikespd']
+    
+    #turn stops file to gdf
+    stops_file = gpd.GeoDataFrame(stops_file, geometry=gpd.points_from_xy(stops_file['stop_lon'], stops_file['stop_lat']), crs='epsg:4326')
+    stops_file.to_crs('epsg:2240',inplace=True)
+    
+    #record starting time
+    time_start = time.time()
+    
+    if trips.shape[0] == 0:
+        print(f'No trips possible from {trip}')
+    else:
+    
+        #get start taz name
+        taz_name = str(trips['src_taz'][0])
+        
+        #get actual first leg time
+        trips['first_leg'] = pd.to_timedelta(trips['first_leg'] / 5280 / bikespd * 60 * 60, unit='s')
+    
+        #find actual final leg time
+        trips['last_leg'] = pd.to_timedelta(trips['last_leg'] / 5280 / bikespd * 60 * 60, unit='s')
+    
+        #get first arrival time (needs to be actual time)
+        trips['arrival_time'] = start_time + trips['first_leg']
+    
+        #create trip dict to put final results in
+        trip_dict = {}
+    
+        #if trips is empty check    
+        if trips.shape[0] > 0:
+            #TODO find more efficient way to do this
+            for idx, row in tqdm(trips.iterrows(),total=trips.shape[0]):
+              
+                SOURCE = int(row['src_stop'])
+                DESTINATION = int(row['dest_stop'])
+                D_TIME = row['arrival_time']
+                
+                output, pareto_optimal = raptor(SOURCE, DESTINATION, D_TIME, raptor_settings['MAX_TRANSFER'], 
+                                raptor_settings['WALKING_FROM_SOURCE'], raptor_settings['CHANGE_TIME_SEC'], raptor_settings['PRINT_ITINERARY'],
+                                routes_by_stop_dict, stops_dict, stoptimes_dict, footpath_dict, idx_by_route_stop_dict)
+                
+
+                if pareto_optimal != None:
+                    #get number of transfers
+                    num_transfers = pareto_optimal[0][0]
+                    
+                    #always take one with fewest transfers
+                    pareto_optimal = pareto_optimal[0][1]
+                
+                    #initialize edge list
+                    edge_list = []
+                
+                    #create edge list
+                    #structure
+                    #leg[0] : walking or the boarding time if transit
+                    #leg[1] : starting transit stop
+                    #leg[2] : ending transit stop
+                    #leg[3] : if walking = travel time and if transit = egress time
+                    #leg[4] : only for transit = route and trip number
+                    
+                    #get rid of last walking leg
+                    while pareto_optimal[-1][0] == 'walking':
+                        pareto_optimal = pareto_optimal[:-1]
+                    
+                    for leg in pareto_optimal:
+                        #format if walking edge
+                        if leg[0] != 'walking':
+                            #create transit edge
+                            #lookup transit mode from route
+
+                            #extract route_id
+                            route_id = str.split(leg[4],'_')[0]
+                            
+                            #get transit type
+                            route_type = route[route['new_route_id'].astype(str)==route_id]['route_type'].item()        
+                            
+                            if (route_type == 1) | (route_type == '1'):
+                                tup = (leg[1],leg[2],leg[3]-leg[0],leg[4],'rail')
+                            elif (route_type == 3) | (route_type == '3'):
+                                tup = (leg[1],leg[2],leg[3]-leg[0],leg[4],'bus')
+                                
+                        else:
+                            #get the arrival time
+                            tup = (leg[1], leg[2], leg[3], 'walking')
+                            #tup = (leg[1],leg[2],leg[3].total_seconds(),'walking')                
+                        edge_list.append(tup)
+            
+                    #get total transit travel time (final egress - first arrival)        
+                    transit_time = pareto_optimal[-1][3] - D_TIME
+                    
+                    #get total travel time from start (fianl arrival time - departure time)
+                    travel_time = pareto_optimal[-1][3] + row['last_leg'] - start_time
+                    
+                    #toss out if two bus routes used
+                    if len([x for x in edge_list if x[-1] == 'bus']) < 2:
+                        #if the key already exists, check to see if lower travel time/transfers
+                        if (row['src_taz'],row['dest_taz']) in set(trip_dict.keys()):
+                            #if lower travel time, replace
+                            if (travel_time < trip_dict[(row['src_taz'],row['dest_taz'])][2]):
+                                # #select the one with fewer transfers
+                                # if (num_transfers <= trip_dict[(row['src_taz'],row['dest_taz'])][-1]):
+                                #add a transfers requirement here
+                                trip_dict[row['src_taz'],row['dest_taz']] = (edge_list,transit_time,travel_time,SOURCE,DESTINATION,num_transfers)
+                        else:
+                            trip_dict[row['src_taz'],row['dest_taz']] = (edge_list,transit_time,travel_time,SOURCE,DESTINATION,num_transfers)
+                                
+            #get trips that aren't feasible with transit 
+            not_possible = [x for x in set(trips.dest_taz) if x not in [x[1] for x in trip_dict.keys()]]
+            
+            #get run time
+            run_time_transit = round(((time.time() - time_start)/60), 2)        
+            metadata = {'not_possible':not_possible,'run_time':run_time_transit}
+                    
+            return trip_dict, metadata
 
     return output
             
@@ -249,12 +366,9 @@ raptor_settings = {
     'first_time': datetime(2022, 11, 24, 9, 0, 0, 0), #9am
     'end_time': datetime(2022, 11, 24, 10, 15, 0, 0), #10am
     'timestep': timedelta(minutes=20), #in minutes 
-    'timelimit': timedelta(hours=1), # in hours
+    'timelimit': timedelta(hours=2), # in hours
     'impedance':'dist',
-    'mode':walk,
-    'one_trip':True,
-    'start':'553',
-    'end':'642'
+    'mode':walk
     }
             
 impedance = raptor_settings['impedance']
@@ -265,7 +379,18 @@ trips_dir = os.fspath(rf'C:/Users/tpassmore6/Documents/TransitSimData/Data/Outpu
 select_tazs = ['288','553','411','1071']
 all_trips = [ trips_dir + rf'/{taz}.parquet' for taz in select_tazs]
 
-run_raptor(all_trips,impedance,raptor_settings)
+start = '553'
+end = '642'
+start_time = datetime(2022, 11, 24, 9, 0, 0, 0)
+
+#import trips
+all_trips = pd.read_parquet(trips_dir + rf'/{taz}.parquet')
+
+#filter
+all_trips = all_trips.loc[all_trips['start_taz']==start and all_trips['end_taz']==end,:]
+
+#run
+trip_dict, metadata = one_trip(trips,impedance,raptor_settings,start_time):
 
 #%% settings
 
